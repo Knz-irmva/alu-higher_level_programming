77f963b3fac05231c8ca00f416007aa63b4d6bd9
Kenzo Irumva <k.irumva@alustudent.com>
2023-07-18 10:36:13 -0700

    task 3

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#3-print_reversed_list_integer.py
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
+
