3027272e4837328f14330006ae4e542244713028
Kenzo Irumva <k.irumva@alustudent.com>
2023-07-18 10:44:42 -0700

    task 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+#5-no_c.py
+
+
+def no_c(my_string):
+    result = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
+
