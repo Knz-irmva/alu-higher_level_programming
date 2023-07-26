0ee162499629848d77942bc878ff0f385d5d6a7a
Kenzo Irumva <k.irumva@alustudent.com>
2023-07-26 08:44:49 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+
+class Square:
+    """
+    Represent a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size):
+        self.__size = size
+
