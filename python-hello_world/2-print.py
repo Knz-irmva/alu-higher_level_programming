2e7c21fad7bdca1a57e8fd4af974b11fe9b396a5
Kenzo Irumva <k.irumva@alustudent.com>
2023-08-03 08:12:30 -0700

    task 2

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def print_phrase():
-    print("Programming is like building a multilingual puzzle")
+print("Programming is like building a multilingual puzzle")
 
-print_phrase()
