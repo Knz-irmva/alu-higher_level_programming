ba836573bc5a2d63326bda7ee2b82179d9353a46
root <k.irumva@alustudent.com>
2023-11-15 02:40:01 -0800

    task 0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import random
-from tqdm.notebook import tqdm
 
-# Simulate the program execution multiple times to demonstrate its functionality
-for _ in tqdm(range(5), desc='Simulating program execution'):
-    number = random.randint(-100, 100)
-    if number > 0:
-        print(f'{number} is positive\n')
-    elif number == 0:
-        print(f'{number} is zero\n')
-    else:
-        print(f'{number} is negative\n')
+# Assign a random signed number to the variable number
+number = random.randint(-100, 100)
+
+# Print the assigned number
+print(f'The number {number}', end=' ')
+
+# Check if the number is positive, zero, or negative
+if number > 0:
+    print('is positive')
+elif number == 0:
+    print('is zero')
+else:
+    print('is negative')
+
