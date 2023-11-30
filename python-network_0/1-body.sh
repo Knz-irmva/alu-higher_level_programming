8c4fe308d81ae8624a31e6bf2dfc2cec0a3d71ae
root <k.irumva@alustudent.com>
2023-11-30 17:27:22 +0000

    task 1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 # Check if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
+[ $# -eq 0 ] && { echo "Usage: $0 <URL>"; exit 1; }
 
-url="$1"
-
-# Use curl to send a GET request and display the body if the status code is 200
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-if [ "$response" -eq 200 ]; then
-    body=$(curl -s "$url")
-    echo "Response Body:"
-    echo "$body"
-else
-    echo "Request failed with status code: $response"
-fi
+# Use curl to send a GET request and display the body for a 200 status code response
+curl -s -w "%{http_code}" "$1" | awk 'NR>1 {print} END {exit !/^200/}'
 
